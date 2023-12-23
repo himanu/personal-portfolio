58a9ddf53fbe3c171402a077162e6adcf352536e
Himanshu Yadav <himanshu.yadav@aknamed.com>
2023-12-23 23:31:57 +0530

    Contact page

---
+++ app/contact/page.js
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { SiGmail } from "react-icons/si";
+import { FaTwitter } from "react-icons/fa";
+export default function Contact() {
+    return (
+        <div className="text-slate-400 flex justify-around h-[50vh] items-center">
+            <div className="text-2xl hover:text-white">
+                <Link href="https://www.linkedin.com/in/himanshu-yadav-7554161b2/" target="_blank" className="flex items-center gap-4">
+                    <IoLogoLinkedin />
+                    <span className="text-sm"> LinkedIn </span>
+                </Link>
+            </div>
+            <div className="text-2xl hover:text-white">
+                <Link href="mailto:message22himanshu@gmail.com" target="_blank" className="flex items-center gap-4">
+                    <SiGmail />
+                    <span className="text-sm"> Mail </span>
+                </Link>
+            </div>
+            <div className="text-2xl hover:text-white">
+                <Link href="https://twitter.com/Himansh45730152" target="_blank" className="flex items-center gap-4">
+                    <FaTwitter />
+                    <span className="text-sm"> Twitter </span>
+                </Link>
+            </div>
+        </div>
+    )
+};
--- app/page.js
+++ app/page.js
@@ -1,10 +1,36 @@
-import Image from 'next/image'
+"use client"
 
+import { useEffect, useState } from "react";
+
+const useTypewriter = (text, speed = 50) => {
+  const [displayText, setDisplayText] = useState(' ');
+
+  useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayText(prevText => prevText + text.charAt(i));
+        i++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [text, speed]);
+
+  return displayText;
+}; 
+
+const text = " A Software Engineer with 2+ years of professional experience building scalable and robust production-ready applications."
 export default function Home() {
+  const content = useTypewriter(text, 50)
   return (
     <main className="absolute top-0 flex min-h-screen flex-col justify-center">
       <div className='text-5xl'> Hii, I am Himanshu</div>
-      <div className='mt-8'>I am a Software Engineer with 2+ years of professional experience building scalable and robust production-ready applications.</div>
+      <div className='mt-8 mr-4'>{content}</div>
     </main>
   )
 }
