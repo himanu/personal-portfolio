34bd004fc182d1317e6eb092c21659947cf9cfe4
Himanshu Yadav <himanshu.yadav@aknamed.com>
2023-12-23 21:50:52 +0530

    completed projects section

--- app/about/projects.js
+++ app/about/projects.js
@@ -2,31 +2,48 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa6";
-import TaskTrackerImage from "./task-tracker.png";
-import { useState } from "react"; 
+import { useState } from "react";
 import { FaCircleArrowRight, FaCircleArrowLeft } from "react-icons/fa6";
 
 const projects = [{
-    name: "task-tracker",
+    name: "Task-Tracker",
     title: "It's a task tracker web app, users can signup using their google account and manage their tasks.",
     github: "https://github.com/himanu/Task-Tracker-React",
     liveDemo: "https://track-tasks-12.netlify.app/",
-    image: TaskTrackerImage
+    image: "/task-tracker.png"
 }, {
-    name: "task-tracker1",
-    title: "It tracker web app, users can signup using their google account and manage their tasks.",
-    github: "https://github.com/himanu/Task-Tracker-React",
-    liveDemo: "https://track-tasks-12.netlify.app/",
-    image: TaskTrackerImage
-}];
+    name: "Codename",
+    title: "Codename is a game played between two teams each having atleast two member. Each team have a master member who will give clues to his fellow members so that they can select correct word in matrix.",
+    github: "https://github.com/himanu/codename",
+    image: "/codename.jpeg",
+}, {
+    name: "Trivia",
+    title: "Trivia is a quiz game played between two teams.",
+    github: "https://github.com/himanu/trivia",
+    image: "/trivia.jpeg",
+}, {
+    name: "Conor Mcgregor Website",
+    title: "Build a replica of Conor mcgregor website",
+    github: "https://github.com/himanu/ConorMcgregor",
+    image: "/conor.png",
+    liveDemo: "https://conormcgregorofficial.herokuapp.com/"
+}, 
+    {
+        name: "Chat App",
+        title: "Built a chat web app",
+        github: "https://github.com/himanu/chat-app",
+        image: "/chat-app.png",
+        liveDemo: "https://make-connection.herokuapp.com/"
+    }
+];
 
 export default function Projects() {
     const [projectIndex, setProjectIndex] = useState(0);
 
-    const moveLeft = () => 
-        setProjectIndex((projectIndex) => projectIndex === 0 ? projects.length -1 : projectIndex - 1);
+    const moveLeft = () =>
+        setProjectIndex((projectIndex) => projectIndex === 0 ? projects.length - 1 : projectIndex - 1);
 
-    const moveRight = () => 
+    const moveRight = () =>
         setProjectIndex((projectIndex) => projectIndex === projects.length - 1 ? 0 : projectIndex + 1);
 
     return (
@@ -44,15 +61,20 @@ export default function Projects() {
                 {/* // eslint-disable-next-line react/jsx-key */}
                 {projects.map((project) => (
                     <div key={project.name} className="flex-col md:flex-row flex gap-10 shrink-0 w-full">
-                        <Image
+                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                        <img
                             src={project.image}
+                            width={200}
+                            height={100}
                             alt="task"
-                            objectFit="contain"
+                            style={{
+                                objectFit: "contain"
+                            }}
                         />
                         <div className="flex flex-col gap-2">
                             <div>
                                 <div className="font-semibold">
-                                    Overview
+                                    {project.name}
                                 </div>
                                 <div className="text-slate-400">
                                     {project.title}
@@ -79,7 +101,7 @@ export default function Projects() {
                             </div>
                         </div>
                     </div>
-                ) )}
+                ))}
             </div>
             {/* Slide arrows */}
             <div className="flex justify-between items-center h-full w-full text-gray-500 text-xl top-0">
